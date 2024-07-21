da866ff3433d771b2d303f1a022944599d7fabd5
Ilyes Nedjai <ilyes.nedjai@pole-emploi.fr>
2024-07-21 22:20:35 +0200

    jenkinfile maj4

--- main.tf
+++ main.tf
@@ -2,4 +2,11 @@ provider "aws" {
   profile = "myTeraaformProfile"
   region  = "us-west-1"  # Remplace par la région que tu utilises
 }
+resource "aws_instance" "foo" {
+	 ami = "ami-05fa00d4c63e32376" # us-west-2
+	 instance_type = "t2.micro"
+	 tags = {
+	 	Name = "TF-Instance"
+ 	}
+}
 
